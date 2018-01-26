9f7b0534dbd35867e017ffdbc7644458423d8089
Florian Didron <florian@111studio.jp>
2018-01-26 17:13:15 +0900

    Adds auth0 config specifics to AI

--- assets/javascripts/auth0.js
+++ assets/javascripts/auth0.js
@@ -1,18 +1,28 @@
 /* global Auth0Lock */
-(function () {
+(function() {
   function appendScript(src, callback) {
     var new_script = document.createElement('script');
-    new_script.setAttribute('src',src);
+    new_script.setAttribute('src', src);
     new_script.onload = callback;
     document.head.appendChild(new_script);
   }
 
-  var lock;
-
   var script_url = '//cdn.auth0.com/js/lock/11.x.y/lock.min.js';
 
-  appendScript(script_url, function () {
-    var checkInterval = setInterval(function () {
+  var Auth0Options = {
+    auth: {
+      redirect: true,
+      redirectUrl: Discourse.SiteSettings.auth0_callback_url,
+      responseType: 'token'
+    },
+    theme: {
+      logo: require('https://storage.googleapis.com/angels-prod/public/platform/logo-social-small.jpg'),
+      primaryColor: '#0e77ca'
+    }
+  };
+
+  appendScript(script_url, function() {
+    var checkInterval = setInterval(function() {
       if (!Discourse.SiteSettings) {
         return;
       }
@@ -22,18 +32,12 @@
       if (!Discourse.SiteSettings.auth0_client_id) {
         return;
       }
-
-      var client_id = Discourse.SiteSettings.auth0_client_id;
-      var domain = Discourse.SiteSettings.auth0_domain;
-
-      lock = new Auth0Lock(client_id, domain);
-
     }, 300);
   });
 
   var LoginController = require('discourse/controllers/login').default;
   LoginController.reopen({
-    authenticationComplete: function () {
+    authenticationComplete: function() {
       if (lock) {
         lock.hide();
       }
@@ -45,24 +49,35 @@
   ApplicationRoute.reopen({
     actions: {
       showLogin: function() {
-        if (!Discourse.SiteSettings.auth0_client_id || Discourse.SiteSettings.auth0_connection !== '') {
+        if (
+          !Discourse.SiteSettings.auth0_client_id ||
+          Discourse.SiteSettings.auth0_connection !== ''
+        ) {
           return this._super();
         }
 
-        lock.show({
-          popup:        true,
-          responseType: 'code',
-          callbackURL:  Discourse.SiteSettings.auth0_callback_url
-        });
+        Auth0Options.initialScreen = 'login;';
+        var lock = new Auth0Lock(
+          Discourse.SiteSettings.auth0_client_id,
+          Discourse.SiteSettings.auth0_domain,
+          Auth0Options
+        );
+
+        lock.show();
 
         this.controllerFor('login').resetForm();
       },
-      showCreateAccount: function () {
-        if (!Discourse.SiteSettings.auth0_client_id || Discourse.SiteSettings.auth0_connection !== '') {
+      showCreateAccount: function() {
+        if (
+          !Discourse.SiteSettings.auth0_client_id ||
+          Discourse.SiteSettings.auth0_connection !== ''
+        ) {
           return this._super();
         }
 
-        var createAccountController = Discourse.__container__.lookup('controller:createAccount');
+        var createAccountController = Discourse.__container__.lookup(
+          'controller:createAccount'
+        );
 
         if (createAccountController && createAccountController.accountEmail) {
           if (lock) {
@@ -72,15 +87,16 @@
             this._super();
           }
         } else {
-          lock.show({
-            mode:         'signup',
-            popup:        true,
-            responseType: 'code',
-            callbackURL:  Discourse.SiteSettings.auth0_callback_url
-          });
+          Auth0Options.initialScreen = 'signUp;';
+          var lock = new Auth0Lock(
+            Discourse.SiteSettings.auth0_client_id,
+            Discourse.SiteSettings.auth0_domain,
+            Auth0Options
+          );
+
+          lock.show();
         }
       }
     }
   });
-
 })();
